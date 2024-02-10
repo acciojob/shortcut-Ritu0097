8e09c2b5fa24e7055f291a6673c5147eca884cc4
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-10 23:41:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function shortcut(s1, s2) {
   // your code here
+	for(let i=0;i<s1.length;i++){
+		return s1[0];
+	}
+	for(let i=0;i<s2.length;i++){
+		return s2[0];
+	}
+	if(s1||s2===""){
+		return "";
+	}
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(shortcut(s1, s2));
